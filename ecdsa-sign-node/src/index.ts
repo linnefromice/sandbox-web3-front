209bd758aa86a9d6a479c8985f289ffd72ac4227
linnefromice <linnefromice@gmail.com>
2023-04-10 23:39:20 +0900

    clean

--- ecdsa-sign-node/src/index.ts
+++
@@ -1,7 +0,0 @@
-const main = async () => {
-  console.log("Main")
-}
-
-main()
-  .then(_ => console.log("Finished!!"))
-  .catch(_ => console.log("Error!!"))
---
+++ ecdsa-sign-node/src/node-crypto.ts
@@ -0,0 +1,36 @@
+const main = async () => {
+  let crypto = await import('node:crypto');
+
+  const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+  });
+  console.log("> privateKey")
+  console.log(privateKey.export({
+    type: 'pkcs1',
+    format: "pem"
+  }))
+  console.log("> publicKey")
+  console.log(publicKey.export({
+    type: 'pkcs1',
+    format: "pem"
+  }))
+
+  const data = Buffer.from('Hello, world!');
+  const hash = crypto.createHash('SHA256').update(data).digest();
+
+  // sign
+  const sign = crypto.createSign('SHA256');
+  sign.update(hash);
+  sign.end();
+  const signature = sign.sign(privateKey, 'base64');
+  console.log("> signature")
+  console.log(signature)
+
+  // verify
+  const verify = crypto.createVerify('sha256').update(hash).verify(publicKey, signature, 'base64');
+  console.log('Verification:', verify);
+}
+
+main()
+  .then(_ => console.log("Finished!!"))
+  .catch(_ => console.log("Error!!"))
