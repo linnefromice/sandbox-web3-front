17ba1293274121c29b4bcb2949ad1a5ceec76a86
linnefromice <linnefromice@gmail.com>
2023-04-19 17:32:51 +0900

    update: add abis & use typechain & call multicall as sample

---
+++ uniswap-node/src/constants.ts
@@ -0,0 +1,46 @@
+import {AddressType, ChainsInfoType, TokenInfoType} from './types';
+
+export const CHAINS = [
+  'ethereum',
+  'polygon',
+  'optimism',
+  'arbitrum',
+  'bnb',
+] as const;
+export const TOKEN_SYMBOLS = [
+  'weth',
+  'wbtc',
+  'usdc',
+  'usdt',
+  'dai',
+  'matic',
+  'link',
+] as const;
+
+const MULTICALL_ADDR: AddressType =
+  '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696';
+export const MULTICALL_ADDRS: ChainsInfoType<AddressType | null> = {
+  ethereum: MULTICALL_ADDR,
+  polygon: MULTICALL_ADDR,
+  optimism: null,
+  arbitrum: null,
+  bnb: null,
+};
+export const ETHEREUM_TOKENS: TokenInfoType<AddressType | null> = {
+  weth: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  wbtc: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+  usdc: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  usdt: '0xdac17f958d2ee523a2206206994597c13d831ec7',
+  dai: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  matic: '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0',
+  link: '0x514910771af9ca656af840dff83e8264ecf986ca',
+};
+export const POLYGON_TOKENS: TokenInfoType<AddressType | null> = {
+  weth: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+  wbtc: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', // PoS?
+  usdc: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+  usdt: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // PoS?
+  dai: '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', // PoS?
+  matic: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+  link: '0x53e0bca35ec356bd5dddfebbd1fc0fd03fabad39',
+};
--- uniswap-node/src/index.ts
+++ uniswap-node/src/index.ts
@@ -1,24 +1,53 @@
-console.log('Try npm run lint/fix!');
+import {ethers} from 'ethers';
+import {ETHEREUM_TOKENS, MULTICALL_ADDRS} from './constants';
+import {TokenInfoType, TokenType} from './types';
+import {ERC20__factory, Multicall2__factory} from './types/typechain';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const main = async () => {
+  // sample: get metatada of tokens
+  const provider = new ethers.providers.JsonRpcProvider(
+    'https://eth-mainnet.g.alchemy.com/v2/JVUDgQSB0r-3HhohPCod6uBy_Zx8WEdy'
+  );
+  const multicallAddr = MULTICALL_ADDRS.ethereum;
+  if (!multicallAddr) throw new Error('Multicall address not found');
+  const multicall = Multicall2__factory.connect(multicallAddr, provider);
+  const iERC20 = ERC20__factory.createInterface();
+  const tokens = ETHEREUM_TOKENS;
+  const calls = Object.entries(tokens)
+    .map(([symbol, addr]) => {
+      if (!addr) throw new Error(`Token address not found: ${symbol}`);
+      return [
+        {
+          target: addr,
+          callData: iERC20.encodeFunctionData('name'),
+        },
+        {
+          target: addr,
+          callData: iERC20.encodeFunctionData('symbol'),
+        },
+        {
+          target: addr,
+          callData: iERC20.encodeFunctionData('decimals'),
+        },
+      ];
+    })
+    .flat();
+  const {returnData} = await multicall.callStatic.aggregate(calls);
+  const calledFuncCounts = 3;
 
-const trailing = 'Semicolon';
+  const results = Object.keys(tokens).reduce((obj, key, idx) => {
+    const from = idx * calledFuncCounts;
+    const chunk = returnData.slice(from, from + calledFuncCounts);
+    obj[key as TokenType] = {
+      name: iERC20.decodeFunctionResult('name', chunk[0])[0],
+      symbol: iERC20.decodeFunctionResult('symbol', chunk[1])[0],
+      decimals: iERC20.decodeFunctionResult('decimals', chunk[2])[0],
+    };
+    return obj;
+  }, {} as TokenInfoType<any>);
+  console.log(results);
+};
 
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
-}
-// TODO: more examples
+main()
+  .then(_ => console.log('Success!!'))
+  .catch(_ => console.error('Failure!!'));
