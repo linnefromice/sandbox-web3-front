36120f5c801b668642ddf07a6d672f788a88d6f4
linnefromice <linnefromice@gmail.com>
2024-03-01 11:47:59 +0900

    initialize

---
+++ spool-node/src/environments.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const _env = process.env;
+export const envs = {
+  providerUrl: {
+    ethereum: _env.ETHEREUM_PROVIDER_URL || '',
+    ethereum_2: _env.ETHEREUM_PROVIDER_URL_2 || '',
+    polygon: _env.POLYGON_PROVIDER_URL || '',
+    optimism: _env.OPTIMISM_PROVIDER_URL || '',
+    arbitrum: _env.ARBITRUM_PROVIDER_URL || '',
+    bnb: _env.BNB_PROVIDER_URL || '',
+  },
+};
