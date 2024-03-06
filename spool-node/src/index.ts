c08fc3bc28372eceec448cf2381db77cb43c0d56
linnefromice <linnefromice@gmail.com>
2024-03-07 00:19:09 +0900

    update

--- spool-node/src/index.ts
+++ spool-node/src/index.ts
@@ -16,10 +16,10 @@ const main = async () => {
   const spoolLensAddr = '0x8aa6174333F75421903b2B5c70DdF8DA5D84f74F';
   const contractSpoolLens = SpoolLens__factory.connect(spoolLensAddr, provider);
 
-  const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+  // const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
   const vaultSrETH = '0x5d6ac99835b0dd42ed9ffc606170e59f75a88fde';
 
-  const totalSupply = await contractSpoolLens.getSVTTotalSupply(weth);
+  const totalSupply = await contractSpoolLens.getSVTTotalSupply(vaultSrETH);
   const balances =
     await contractSpoolLens.callStatic.getSmartVaultAssetBalances(
       vaultSrETH,
@@ -37,6 +37,7 @@ const main = async () => {
 
   console.log(`totalSupply: ${formatEther(totalSupply)}`);
   console.log(`balances: ${balances.map(b => formatEther(b))}`);
+  console.log(`rawPrice: ${rawPrice}`);
   console.log(`price: ${price}`);
   // Val SVT = price * balances[0] / totalSupply
 };
